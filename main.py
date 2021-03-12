8211880df1bb6dade7026eeceb231216999722a9
Vinicius Bruscagini <vinibrenobr11@hotmail.com>
2021-03-11 21:33:19 -0300

    Finish model training

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import json
-from flask import Flask, jsonify
+import joblib
+from flask import Flask, jsonify, Response
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return jsonify({"Classic": "Hello World"})
+@app.route("/checkEmail", methods=['POST'])
+def main():
+
+    model = joblib.load("model/model.pkl")
+
+    return Response(status=501)
+    
 
 app.run()
