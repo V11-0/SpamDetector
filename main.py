847cb2a52c1be2465af4e0a87bf5949d7efb1394
Vinicius Bruscagini <vinibrenobr11@hotmail.com>
2021-03-17 22:25:52 -0300

    Transform email in data for model

--- main.py
+++ main.py
@@ -1,40 +1,148 @@
+import numpy as np
 import joblib
+import re
 from flask import Flask, jsonify, Response, request
 
 app = Flask(__name__)
 
 words_frequency = []
 char_frequency = []
+capital_data = []
+number_words = 0
+number_chars = 0
+message = ''
 
+# Things: We assume message does not have line breaks 
 @app.route("/checkEmail", methods=['POST'])
 def checkEmail():
 
     model = joblib.load("model/model.pkl")
 
+    global message
     message = request.json['message']
         
     if len(message) == 0:
         return Response(status=400)
 
-    # Count Words
+    # Count capitals related things
+    countCapitalLettersSequences()
 
-    return message
+    message = message.lower()
+
+    # Count number of words and chars
+    message_words = message.split(" ")
+
+    global number_words, number_chars
+    number_words = len(message_words)
+    number_chars = len(message)
+
+    # Count Amount of each word and chars defined in the dataset
+    countDatasetWords(message_words)
+    countDatasetChars()
+
+    # Until here we have the absolute frquency of words and chars, we need to get this data in percentages
+    final_data = toModelDataFormat()
+    
+    # Add columns names as np array, as in the last section of model.ipynb
+
+    return model.predict(final_data)
+    
+
+def countDatasetWords(message_words):
+
+    global words_frequency
+
+    for mword in message_words:
+        for index, word in enumerate(words_frequency):
+            if (word == mword):
+                words_frequency[index][1] += 1
+
+def countDatasetChars():
+
+    global char_frequency
+    
+    for mchar in message:
+        for index, char in enumerate(char_frequency):
+            if (mchar == char):
+                char_frequency[index][1] += 1
+
+def countCapitalLettersSequences():
+
+    capital_sequences = []
+
+    for index, char in enumerate(message):
+        if (char.isupper()):
+            if (message[index - 1].isupper()):
+                capital_sequences[-1] + char
+            else:
+                capital_sequences.append(char)
+
+    # Get the data we want
+
+    # Average length of uninterrupted sequences of capital letters
+    global capital_data
+    len_sum = 0
+
+    for sequence in capital_sequences:
+        len_sum = len(sequence)
+
+    len_avg = len_sum / len(capital_sequences)
+    capital_data.append(len_avg)
+
+    # Length of longest uninterrupted sequence of capital letters
+    len_longest = 0
+
+    for sequence in capital_sequences:
+        if (len(sequence) > len_longest):
+            len_longest = len(sequence)
+
+    capital_data.append(len_longest)
+
+    # total number of capital letters in the e-mail, we already have that
+    capital_data.append(len_sum)
+
+def toModelDataFormat():
+
+    email = []
+    
+    # To percentage frequency and add to email model
+    for word_freq in words_frequency:
+        email.append(word_freq[1] / number_words)
+
+    for char_freq in char_frequency:
+        email.append(char_freq[1] / number_chars)
+
+    # We add the capital letters data
+    for data in capital_data:
+        email.append(data)
+
+    return email
     
 def main():
+
+    global words_frequency, char_frequency
     
     # Define in an array the words used by the database
     words_frequency = [
-        'make', 'address', 'all', '3d', 'our', 'over', 'remove', 'internet',
-        'order', 'mail', 'receive', 'will', 'people', 'report', 'addresses',
-        'free', 'business', 'email', 'you', 'credit', 'your', 'font', '000',
-        'money', 'hp', 'hpl', 'george', '650', 'lab', 'labs', 'telnet', '857',
-        'data', '415', '85', 'technology', '1999', 'parts', 'pm', 'direct', 'cs',
-        'meeting', 'original', 'project', 're', 'edu', 'table', 'conference'
+        ['make'], ['address'], ['all'], ['3d'], ['our'], ['over'], ['remove'], ['internet'],
+        ['order'], ['mail'], ['receive'], ['will'], ['people'], ['report'], ['addresses'],
+        ['free'], ['business'], ['email'], ['you'], ['credit'], ['your'], ['font'], ['000'],
+        ['money'], ['hp'], ['hpl'], ['george'], ['650'], ['lab'], ['labs'], ['telnet'], ['857'],
+        ['data'], ['415'], ['85'], ['technology'], ['1999'], ['parts'], ['pm'], ['direct'], ['cs'],
+        ['meeting'], ['original'], ['project'], ['re'], ['edu'], ['table'], ['conference']
     ]
 
-    # And the chars
-    char_frequency = [';', '(', '[', '!', '$', '#']
+    # Define the second index of each set the number of occurrences of word, we start with 0
+    for word_data in words_frequency:
+        word_data.append(0)
 
+    # And the chars, same thing
+    char_frequency = [[';'], ['('], ['['], ['!'], ['$'], ['#']]
+
+    for char_data in char_frequency:
+        char_data.append(0)
+
+    
     app.run()
 
 main()
